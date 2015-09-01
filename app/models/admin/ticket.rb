509ec797b9b7880b4b16c35a6c859a62da97105d
Guilherme <guilherme.cortes@handcom.com.br>
2015-09-01 16:43:25 -0300

    Descomentado modelo Ticket

--- app/models/admin/ticket.rb
+++ app/models/admin/ticket.rb
@@ -4,12 +4,12 @@ class Admin::Ticket < ActiveRecord::Base
     belongs_to :admin_category, :class_name => 'Admin::Category'
     belongs_to :admin_work_type, :class_name => 'Admin::WorkType'
 
-    # validates :subject, :presence => {:message => "Campo obrigatório"}
-    # validates :status, :presence => {:message => "Campo obrigatório"}
-    # validates :priority, :presence => {:message => "Campo obrigatório"}
-    # validates :planned, :inclusion => {:in => [true, false]}
-    # validates_associated :user
-    # validates_associated :admin_category
-    # validates_associated :admin_work_type
+    validates :subject, :presence => {:message => "Campo obrigatório"}
+    validates :status, :presence => {:message => "Campo obrigatório"}
+    validates :priority, :presence => {:message => "Campo obrigatório"}
+    validates :planned, :inclusion => {:in => [true, false]}
+    validates_associated :user
+    validates_associated :admin_category
+    validates_associated :admin_work_type
 
 end
