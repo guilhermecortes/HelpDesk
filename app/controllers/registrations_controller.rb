d5e9fdedda1d822614544073c7004ad60680b8db
Guilherme <guilherme.cortes@handcom.com.br>
2015-09-10 11:37:25 -0300

    Alteração método create do controller registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -42,20 +42,22 @@ class RegistrationsController < Devise::RegistrationsController
 		resource_saved = resource.save
 		yield resource if block_given?
 		if resource_saved
-		  if resource.active_for_authentication?
-		    set_flash_message :notice, :signed_up if is_flashing_format?
-		    # sign_up(resource_name, resource)
-		    # respond_with resource, location: after_sign_up_path_for(resource)
-			flash[:estado] = "Inserido-sucesso"
-			redirect_to admin_usuarios_path
-		  else
-		    set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-		    expire_data_after_sign_in!
-		    respond_with resource, location: after_inactive_sign_up_path_for(resource)
-		  end
+			active_resource resource
 		else
 		  clean_up_passwords resource
 		  respond_with resource
+		end
+	end
+
+	def active_resource resource
+		if resource.active_for_authentication?
+			set_flash_message :notice, :signed_up if is_flashing_format?
+			flash[:estado] = "Inserido-sucesso"
+			redirect_to admin_usuarios_path
+		else
+			set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+			expire_data_after_sign_in!
+			respond_with resource, location: after_inactive_sign_up_path_for(resource)
 		end
 	end
 
