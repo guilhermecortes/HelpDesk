6033e225a0a63757299fe152dc4d34120a112c69
Guilherme <guilherme.cortes@handcom.com.br>
2015-09-10 11:59:39 -0300

    Refatoração passwords_controller

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -27,16 +27,18 @@ class PasswordsController < Devise::PasswordsController
         self.resource = resource_class.reset_password_by_token(resource_params)
 
         if resource.errors.empty?
-            resource.unlock_access! if unlockable?(resource)
-            flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-            set_flash_message(:notice, flash_message) if is_navigational_format?
-            sign_in(resource_name, resource)
-            #TODO alterar
-            # redirect_to minha_conta_path, :notice => "Password has been change"
-            redirect_to admin_usuarios_path, :notice => "Password has been change"
+            resource_update resource
         else
             respond_with resource
         end
+    end
+
+    def resource_update resource
+        resource.unlock_access! if unlockable?(resource)
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message(:notice, flash_message) if is_navigational_format?
+        sign_in(resource_name, resource)
+        redirect_to admin_usuarios_path, :notice => "Password has been change"
     end
 
     protected
